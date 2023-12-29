545e28af19389cec121aa2d11006237511e77f52
Charles Wang <charleswang007@gmail.com>
2023-12-30 01:34:35 +0800

    add mouse event

--- js/basketball.js
+++ js/basketball.js
@@ -17,7 +17,7 @@ var Score = function (){
 Score.prototype.draw = function (ctx) {
 	ctx.font = "22px Arial";
     ctx.fillStyle = 'white';
-    ctx.fillText("您的關鍵一投 (空白鍵)，攸關台灣未來。", 35, 765);
+    ctx.fillText("您的關鍵一投 (空白鍵 or 滑鼠左鍵)，攸關台灣未來。", 35, 765);
 	ctx.font = "22px Arial";
     ctx.fillStyle = 'white';
     ctx.fillText("Score = " + this.playerScore, 370, 550);
